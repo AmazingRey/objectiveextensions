30f8d65b55a9bee460b85427494c093552a0e6f9
Gertjan Leemans <gertjan@digitalisma.nl>
2012-11-28 15:35:08 +0100

    Added UIImage+fixOrientation

---
+++ UIImage+fixOrientation/UIImage+fixOrientation.h
@@ -0,0 +1,15 @@
+//
+//  UIImage+fixOrientation.h
+//  Gertjan Leemans
+//
+//  Created by Gertjan Leemans on 03/04/12.
+//  Copyright (c) 2012 Gertjan Leemans. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (fixOrientation)
+
+- (UIImage *)fixOrientation;
+
+@end
