c232c2a56dc6ef3605e2599be74790c9acd87b46
Gertjan Leemans <gertjan@digitalisma.nl>
2012-11-28 15:37:30 +0100

    Added NSString+MD5

---
+++ NSString+MD5/NSString+MD5.h
@@ -0,0 +1,15 @@
+//
+//  NSString_MD5.h
+//  Gertjan Leemans
+//
+//  Created by Gertjan Leemans on 22/05/12.
+//  Copyright (c) 2012 Gertjan Leemans. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString(MD5)
+
+- (NSString *) MD5;
+
+@end
