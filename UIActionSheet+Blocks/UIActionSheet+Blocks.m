67c739543f5e254755afeaa3d1ebca892192725d
Gertjan Leemans <gertjan@digitalisma.nl>
2012-11-28 15:57:16 +0100

    Added MIT License

--- UIActionSheet+Blocks/UIActionSheet+Blocks.m
+++ UIActionSheet+Blocks/UIActionSheet+Blocks.m
@@ -1,15 +1,20 @@
 //
+//  Copyright (c) 2012 Gertjan Leemans
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
 //  UIActionSheet+Blocks.m
 //  Gertjan Leemans
 //
 //  Created by Gertjan Leemans on 28/11/12.
-//  Copyright 2012 Gertjan Leemans, LLC. All rights reserved.
 //
 
 #import "UIActionSheet+Blocks.h"
 #import <objc/runtime.h>
 
-static NSString *GL_BUTTON_ASS_KEY = @"nl.gertjanleemans.UIActionSheet+Blocks";
+static NSString *kButtonKey = @"nl.gertjanleemans.UIActionSheet+Blocks";
 
 @implementation UIActionSheet (Blocks)
 
@@ -50,13 +55,13 @@ static NSString *GL_BUTTON_ASS_KEY = @"nl.gertjanleemans.UIActionSheet+Blocks";
             [self setCancelButtonIndex:cancelIndex];
         }
         
-        objc_setAssociatedObject(self, (__bridge const void *)(GL_BUTTON_ASS_KEY), buttonsArray, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(self, (__bridge const void *)(kButtonKey), buttonsArray, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
     return self;
 }
 
 - (NSInteger) addButtonItem:(UIActionSheetButtonItem *)item{	
-    NSMutableArray *buttonsArray = objc_getAssociatedObject(self, (__bridge const void *)(GL_BUTTON_ASS_KEY));	
+    NSMutableArray *buttonsArray = objc_getAssociatedObject(self, (__bridge const void *)(kButtonKey));	
 	
 	NSInteger buttonIndex = [self addButtonWithTitle:item.label];
 	[buttonsArray addObject:item];
@@ -66,12 +71,12 @@ static NSString *GL_BUTTON_ASS_KEY = @"nl.gertjanleemans.UIActionSheet+Blocks";
 
 - (void) actionSheet: (UIActionSheet *) actionSheet didDismissWithButtonIndex: (NSInteger) buttonIndex{
     if (buttonIndex != -1){
-        NSArray *buttonsArray = objc_getAssociatedObject(self, (__bridge const void *)(GL_BUTTON_ASS_KEY));
+        NSArray *buttonsArray = objc_getAssociatedObject(self, (__bridge const void *)(kButtonKey));
         UIActionSheetButtonItem *item = [buttonsArray objectAtIndex:buttonIndex];
         if(item.action){
             item.action();
         }
-        objc_setAssociatedObject(self, (__bridge const void *)(GL_BUTTON_ASS_KEY), nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+        objc_setAssociatedObject(self, (__bridge const void *)(kButtonKey), nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
     }
 }
 
