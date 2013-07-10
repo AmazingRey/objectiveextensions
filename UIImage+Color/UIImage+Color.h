f37b6831359af8dbe53b77d5360a478888061f1d
Gertjan Leemans <gertjan@digitalisma.com>
2013-07-10 08:26:59 +0200

    Added UIImage+Color, to create an overlaid image

---
+++ UIImage+Color/UIImage+Color.h
@@ -0,0 +1,15 @@
+//
+//  UIImage_Color.h
+//  Belle Amie
+//
+//  Created by Gertjan Leemans on 09/07/13.
+//  Copyright (c) 2013 Digitalisma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIImage (Color)
+
+- (UIImage*) imageWithColor: (UIColor*) color;
+
+@end
